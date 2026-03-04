d3e6d75b7e28b4ae10b057d4af76f2e18e816be7
idreamshen <idream.shen@gmail.com>
2026-03-04 14:43:25 +0000

    chore: add nix

---
+++ .nix/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            esphome
+            esptool
+          ];
+
+          shellHook = ''
+            echo "ESPHome development environment loaded"
+            echo "ESPHome version: $(esphome version)"
+          '';
+        };
+      }
+    );
+}
